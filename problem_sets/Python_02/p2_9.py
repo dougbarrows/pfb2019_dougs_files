e93a7d486338ed9e6ea5f4908aca5d7994e65dd6
info <info@info11.cshl.edu>
2019-10-16 16:00:15 -0400

    problem 2.9 done

---
+++ problem_sets/Python_02/p2_9.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+number = 30
+
+if number > 0:
+	print("positive")
+	if number < 50:
+		print(" and less than 50")
+elif number < 0:
+	print("negative")
+else:
+	print("its zero")
