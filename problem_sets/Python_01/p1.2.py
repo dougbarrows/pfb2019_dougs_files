4184f17aa885b4fbdb66557ff1994307e7f8bd1a
info <info@info11.cshl.edu>
2019-10-16 10:52:41 -0400

    added more stuff

--- problem_sets/Python_01/p1.2.py
+++ problem_sets/Python_01/p1.2.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
+import sys
 
 print("My name: Doug")
+print("My favorite color: blue")
+
+fav_activity = "python"
+print("My favorite activity:", fav_activity)
+
+print("My favorite animal: cat")
+
+print("done with system arguments!!")
+
+print("My name:", sys.argv[1])
+print("My favorite color:", sys.argv[2])
+print("My favorite activity:", sys.argv[3])
+
+print("My favorite animal:", sys.argv[4])
