577d744ca4e3add539de25724e0edbfa860c9c29
info <info@info11.cshl.edu>
2019-10-16 15:22:29 -0400

    done with python1

--- problem_sets/Python_01/p1.2.py
+++ problem_sets/Python_01/p1.2.py
@@ -2,18 +2,24 @@
 
 import sys
 
+
+print("\nSimple, just typing it all out \n")
 print("My name: Doug")
 print("My favorite color: blue")
 
 fav_activity = "python"
 print("My favorite activity:", fav_activity)
 
-print("My favorite animal: cat")
+print("My favorite animal: cat \n")
 
-print("done with system arguments!!")
+print("done with system arguments!! \n")
 
-print("My name:", sys.argv[1])
-print("My favorite color:", sys.argv[2])
-print("My favorite activity:", sys.argv[3])
+print("My name: " +  sys.argv[1])
+print("My favorite color: " + sys.argv[2])
+print("My favorite activity: " + sys.argv[3])
+print("My favorite animal: " + sys.argv[4], "\n")
 
-print("My favorite animal:", sys.argv[4])
+
+print("print all in one print statement! \n")
+
+print("My name:", sys.argv[1], "\n"+ "My favorite color:", sys.argv[2], "\n" +  "My favorite activity:", sys.argv[3], "\n" + "My favorite activity:", sys.argv[4])
