8724c4a820e7e09a704ee8f5b5dcb94c71837718
info <info@info11.cshl.edu>
2019-10-18 11:06:35 -0400

    probmes 5.11 and 5.13

---
+++ problem_sets/Python_05/p5.11.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+setA = { 3, 14, 15, 9, 26, 5, 35, 9}
+setB = { 6, 22, 14, 0, 9}
+
+print(setA & setB)
+print(setA | setB)
+print(setA - setB)
+print(setB - setA) 
+print(setA ^ setB)
+ 
