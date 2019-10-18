3c5519613dfb15127284fde7b0f3679acec5ccd0
info <info@info11.cshl.edu>
2019-10-17 22:04:46 -0400

    problem 12

---
+++ problem_sets/Python_04/p4.12.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+dna = ['ATGCCCGGCCCGGC','GCGTGCTAGCAATACGATAAACCGG', 'ATATATATCGAT','ATGGGCCC']
+
+test = []
+
+test_tup = [test.append((len(i), i)) for i in dna]
+
+print(test)
+
+ 
