c7485e088ac56b958b0da5cfc2c3334b48d9b859
DouglasBarrows <doug.barrows@gmail.com>
2019-10-20 15:16:41 -0400

    work from apartment up through 9.1c

--- problem_sets/Python_08/p8.6.py
+++ problem_sets/Python_08/p8.6.py
@@ -29,16 +29,28 @@ translation_table = {
     'TAA':'*', 'TGA':'*', 'TAG':'*'
 }
 
-with open("Python_08.codons-6frames.nt", "r") as fo, open("Python_08.translated.aa", "w") as prot_write:
+with open("Python_08.codons-6frames.nt", "r") as fo, open("Python_08.translated.aa", "w") as prot_write, open("Python_08.translated-longest.aa", "w") as longest_orf_write:
 	for line in fo:
 		if re.search("codons", line):
 			line = line.rstrip()
 			seq_name = line + "_prot"
 			prot_write.write(seq_name + '\n')
-		else:
+			longest_orf_write.write(seq_name + '\n')
+		elif re.search(r"[\w]", line): # somehow inserted an empty line in between some sequences, so need ot make sure we skip those. 
 			for match in re.finditer(r"([AGCT]{3})", line):
 				line = (re.sub(match.group(1), translation_table[match.group(1)], line))
-			prot_write.write(line) 
+				line = line.replace(" ", "")
+				orfs = re.findall(r"[M][A-Z]*[\*]", line)
+				if len(orfs) > 0:
+					orf_long = sorted(orfs, key = len, reverse = True)[0][0:-1] + '\n'
+				else:
+					orf_long = "no open reading frame\n"
+			prot_write.write(line)
+			longest_orf_write.write(orf_long)
+			 
+
+
+
 		
 
 
---
+++ problem_sets/Python_08/p8.7.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+with open('Python_08.translated_2.aa', 'r'), as fo:
+	
